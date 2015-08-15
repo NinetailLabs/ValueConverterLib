2c4de22ed9d453df8f37fe3024c18ca3a50abd69
Ralph Viktor <ralphviktor@gmail.com>
2015-08-15 16:17:55 +0200

    Initial commit. Includes InvalidDateConverter that allows conversion of DateTime.MaxValue and DateTime.MinValue to a spesified string value for display.

---
+++ NineTailLabs.ValueConverterLib.Tests/InvalidDateConverterTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NineTailLabs.ValueConverterLib.Tests
+{
+    [TestClass]
+    public class InvalidDateConverterTest
+    {
+        /// <summary>
+        /// Test the Convert method when passing a valid DateTime
+        /// </summary>
+        [TestMethod]
+        public void TestConvertValidDate()
+        {
+            var instance = new InvalidDateConverter();
+            var date = DateTime.Now;
+            var retVal = instance.Convert(date, typeof (string), "Unknown", CultureInfo.InvariantCulture);
+            Assert.AreEqual(date.ToString(CultureInfo.InvariantCulture), retVal);
+        }
+
+        /// <summary>
+        /// Test the Convert method when passing a DateTime.MinValue and DateTime.MaxValue
+        /// </summary>
+        [TestMethod]
+        public void TestConvertInvalidDate()
+        {
+            var instance = new InvalidDateConverter();
+            var date = DateTime.MaxValue;
+            var retVal = instance.Convert(date, typeof (string), "Unknown", CultureInfo.InvariantCulture);
+            Assert.AreEqual("Unknown", retVal);
+
+            var minDate = DateTime.MinValue;
+            var minRet = instance.Convert(minDate, typeof (string), "Unknown", CultureInfo.InvariantCulture);
+            Assert.AreEqual("Unknown", minRet);
+        }
+
+        /// <summary>
+        /// Test the Convert method when passing a null date
+        /// </summary>
+        [TestMethod]
+        public void TestConvertNullDate()
+        {
+            var instance = new InvalidDateConverter();
+            var retVal = instance.Convert(null, typeof (string), "Unknown", CultureInfo.InvariantCulture);
+            Assert.AreEqual("{Binding.DoNothing}", retVal.ToString());
+        }
+
+        /// <summary>
+        /// Test the Convert method when passing a null parameter
+        /// </summary>
+        [TestMethod]
+        public void TestConvertNullParameter()
+        {
+            var instance = new InvalidDateConverter();
+            var date = DateTime.MaxValue;
+            var retVal = instance.Convert(date, typeof (string), null, CultureInfo.InvariantCulture);
+            Assert.AreEqual("-", retVal);
+        }
+
+        /// <summary>
+        /// Test the ConvertBack method when passing a valid DateTime string
+        /// </summary>
+        [TestMethod]
+        public void TestConvertBackValidDate()
+        {
+            var instance = new InvalidDateConverter();
+            var date = DateTime.Now;
+            var stringDate = date.ToString(CultureInfo.InvariantCulture);
+            var retVal = instance.ConvertBack(stringDate, typeof (DateTime), "Unknown", CultureInfo.InvariantCulture);
+            var retDate = retVal as DateTime?;
+            Assert.IsNotNull(retDate);
+            Assert.AreEqual(date.ToString(CultureInfo.InvariantCulture), ((DateTime)retDate).ToString(CultureInfo.InvariantCulture));
+        }
+
+        /// <summary>
+        /// Test the ConvertBack method when passing a '-' string
+        /// </summary>
+        [TestMethod]
+        public void TestConvertBackInvalidDate()
+        {
+            var instance = new InvalidDateConverter();
+            var retDate = instance.ConvertBack("Unknown", typeof (DateTime),
+                "Unknown", CultureInfo.InvariantCulture);
+            Assert.AreEqual(DateTime.MinValue, (DateTime)retDate);
+        }
+
+        /// <summary>
+        /// Test the ConvertBack methods when passing a null
+        /// </summary>
+        [TestMethod]
+        public void TestConvertBackNullValue()
+        {
+            var instance = new InvalidDateConverter();
+            var retDate = instance.ConvertBack(null, typeof (DateTime), "Unknown", CultureInfo.InvariantCulture);
+            Assert.AreEqual("{Binding.DoNothing}", retDate.ToString());
+        }
+
+        /// <summary>
+        /// Test the ConvertBack method when passing a null parameter
+        /// </summary>
+        [TestMethod]
+        public void TestConvertBackNullParameter()
+        {
+            var instance = new InvalidDateConverter();
+            var retDate = instance.ConvertBack("-", typeof (DateTime), null, CultureInfo.InvariantCulture);
+            Assert.AreEqual(DateTime.MinValue, (DateTime)retDate);
+        }
+
+        /// <summary>
+        /// Attempt to convert a non-date string back into a DateTime
+        /// </summary>
+        [TestMethod]
+        public void TestNonDateStringConvertBack()
+        {
+            var instance = new InvalidDateConverter();
+            var retDate = instance.ConvertBack("Hello World", typeof (DateTime), null, CultureInfo.InvariantCulture);
+            Assert.AreEqual("{DependencyProperty.UnsetValue}", retDate.ToString());
+        }
+    }
+}
---
+++ NineTailLabs.ValueConverterLib/InvalidDateConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NineTailLabs.ValueConverterLib
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class InvalidDateConverter : IValueConverter
+    {
+        /// <summary>
+        /// Check if a date is valid (not the max or min date) and if it isn't return a value to indicate that the date is unknown
+        /// </summary>
+        /// <param name="value">DateTime value</param>
+        /// <param name="targetType">String</param>
+        /// <param name="parameter">String to return if date is invalid. If blank returns '-'</param>
+        /// <param name="culture">Culture information for the date</param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return Binding.DoNothing;
+
+            var returnValue = "-";
+            if (parameter != null)
+            {
+                returnValue = parameter.ToString();
+            }
+
+            var dateValue = value as DateTime?;
+            if (dateValue != null && dateValue != DateTime.MaxValue && dateValue != DateTime.MinValue)
+            {
+                    returnValue = ((DateTime)dateValue).ToString(culture);
+            }
+
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Check if the date string is valid (not a dash or spesified invalid) and returns it as a date.
+        /// For invalid dates DateTime.MinValue is returned
+        /// </summary>
+        /// <param name="value">Date string</param>
+        /// <param name="targetType">DateTime</param>
+        /// <param name="parameter">String to return if date is invalid. If blank uses '-'</param>
+        /// <param name="culture">Culture information for the date</param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return Binding.DoNothing;
+
+            var returnValue = DateTime.MinValue;
+            var paramValue = "-";
+            if (parameter != null)
+            {
+                paramValue = parameter.ToString();
+            }
+
+            if (value.ToString() == paramValue) return returnValue;
+            try
+            {
+                returnValue = DateTime.Parse(value.ToString(), culture);
+            }
+            catch (Exception)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+            return returnValue;
+        }
+    }
+}
